1c7fb278959c54976b03a22334746428d8afab43
srikanth Karra <srikanthreddy376@gmail.com>
2018-07-02 23:28:21 +0200

    'component_form'

--- src/components/AddStudent.vue
+++ src/components/AddStudent.vue
@@ -2,50 +2,74 @@
 <div id="new-student">
   <h3>Add a New student</h3>
   <div class="row">
-    <form @submit.prevent="saveSrudent" class="col s12">
+    <form @submit.prevent="saveStudent" class="col s12">
       <div class="row">
         <div class="input-field col s12">
           <input type ="text" v-model="student_id" required>
           <label>Student_Id#</label>
         </div>
       </div>
+      <div class="row">
+        <div class="input-field col s12">
+          <input type ="text" v-model="name" required>
+          <label>Student_Name</label>
+        </div>
+        </div>
         <div class="row">
         <div class="input-field col s12">
           <input type ="text" v-model="father_name" required>
-          <label>FatherName</label>
+          <label>Father_Name</label>
         </div>
         </div>
-        <div class="row">
-        <div class="input-field col s12">
-          <input type ="text" v-model="student_id" required>
-          <label>Student_Id#</label>
-        </div>
-        </div>
+        
         <div class="row">
         <div class="input-field col s12">
           <input type ="text" v-model="fee_paid" required>
-          <label>Fee paid</label>
+          <label>Fee_paid</label>
         </div>
         </div>
         <div class="row">
         <div class="input-field col s12">
           <input type ="text" v-model="due_fee" required>
-          <label>Due fee</label>
+          <label>Due_fee</label>
         </div>
         </div>
-      
+         <button type="submit" class="btn  green"> SUBMIT</button>
+         <router-link to="/" class="btn orange">CANCEL</router-link>
     </form>
-    <router-link to="/" class="btn blue">Add</router-link>
+ 
+  
   </div>
 </div>
 </template>
 <script>
+import db from './firebaseinit'
 export default {
   name :'new-student',
   data () {
     return {
+      student_id:null,
+     name:null,
+     father_name:null,
+     Class:null,
+     fee_paid:null,
+     due_fee:null
 
     }
-  }
+    
+  },
+  methods:{
+    saveStudent() {
+      db.collection('Students').add({
+        student_id: this.student_id,
+        name:this.name,
+        father_name:this.father_name,
+        fee_paid:this.fee_paid,
+        due_fee:this.due_fee
+      }).then(docRef=>this.$router.push('/'))
+      .catch(error=>console.log(err))
+    }
+      
+    }
 }
 </script>
