85e65bf3a9a35c24eb7a3af730c9f77063471617
Srikanth Karra <srikanthreddy376@gmail.com>
2018-07-02 23:00:27 +0200

    deleted some xomments

--- src/components/firebaseinit.js
+++ src/components/firebaseinit.js
@@ -3,6 +3,6 @@ import 'firebase/firestore'
 import firebaseConfig from './firebaseConfig'
 const firebaseApp = firebase.initializeApp(firebaseConfig)
 const firestore = firebase.firestore();
-const settings = {/* your settings... */ timestampsInSnapshots: true};
+const settings = { timestampsInSnapshots: true};
 firestore.settings(settings);
 export default firebase.firestore()
